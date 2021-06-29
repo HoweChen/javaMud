59795d7760a71016fb15087c450752aaf65c736b
Yuhao.Chen (Howe) - 陈雨豪 <yuhao.howe.chen@gmail.com>
2021-06-29 23:07:56 +0800

    update code

---
+++ mud-java-basic/src/main/java/juc/SyncVSLockVSAtomicInteger.java
@@ -0,0 +1,10 @@
+package juc;
+
+import cn.hutool.core.text.CharSequenceUtil;
+import org.openjdk.jmh.annotations.Measurement;
+
+@Measurement
+public class SyncVSLockVSAtomicInteger {
+  public static void main(String[] args) {
+  }
+}
--- mud-shiro/src/test/java/HelloWorld/ShiroHelloWorld.java
+++ mud-shiro/src/test/java/HelloWorld/ShiroHelloWorld.java
@@ -1,5 +1,12 @@
 package HelloWorld;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.util.Factory;
+import org.junit.jupiter.api.Test;
+
 import javax.security.auth.Subject;
 
 public class ShiroHelloWorld {
@@ -7,8 +14,7 @@ public class ShiroHelloWorld {
   @Test
   public void testHelloworld() {
     // 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-    Factory<SecurityManager> factory =
-        new IniSecurityManagerFactory("classpath:shiro.ini");
+    Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
     // 2、得到SecurityManager实例 并绑定给SecurityUtils
     org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
     SecurityUtils.setSecurityManager(securityManager);
