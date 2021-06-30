548f56b40d6cd628a6d49d54d952e5c42934a8b1
HoweChen <yuhao.howe.chen@gmail.com>
2021-07-01 00:54:04 +0800

    update code

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-j
-:mud-java-basic/src/main/java/juc/TicketWindowRunnable.java,d\e\deae3a3af6b764d34b572b0050e3b8a751d096b8
-O
-mud-camel/src/data/message1.xml,6\2\625d756f543268b642df5d18410b1ac8e274e4b9
-F
-mud-springboot/pom.xml,7\6\76a95952fa5afd8ce6375fa7b0650ef7af43e7bb
--- mud-java-basic/src/main/java/juc/SyncVSLockVSAtomicInteger.java
+++ mud-java-basic/src/main/java/juc/SyncVSLockVSAtomicInteger.java
@@ -1,10 +1,92 @@
 package juc;
 
-import cn.hutool.core.text.CharSequenceUtil;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Group;
+import org.openjdk.jmh.annotations.GroupThreads;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.profile.StackProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
-@Measurement
+@Measurement(iterations = 10)
+@Warmup(iterations = 10)
+@BenchmarkMode(value = Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class SyncVSLockVSAtomicInteger {
-  public static void main(String[] args) {
+
+  @State(Scope.Group)
+  public static class IntMonitor {
+    private int x;
+    private final Lock lock = new ReentrantLock();
+
+    public void lockInc() {
+      lock.lock();
+      try {
+        x++;
+      } finally {
+        lock.unlock();
+      }
+    }
+
+    public void syncInc() {
+      synchronized (this) {
+        x++;
+      }
+    }
+  }
+
+  @State(Scope.Group)
+  public static class AtomicIntegerMonitor {
+    private AtomicInteger x = new AtomicInteger();
+
+    public void inc() {
+      x.incrementAndGet();
+    }
+  }
+
+  @GroupThreads(10)
+  @Group("lock")
+  @Benchmark
+  public void lockInc(IntMonitor monitor) {
+    monitor.lockInc();
+  }
+
+  @GroupThreads(10)
+  @Group("sync")
+  @Benchmark
+  public void syncInc(IntMonitor monitor) {
+    monitor.syncInc();
+  }
+
+  @GroupThreads(10)
+  @Group("atomic")
+  @Benchmark
+  public void syncInc(AtomicIntegerMonitor monitor) {
+    monitor.inc();
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    Options opts =
+        new OptionsBuilder()
+            .include(SyncVSLockVSAtomicInteger.class.getSimpleName())
+            .forks(1)
+            .timeout(TimeValue.seconds(10))
+            .addProfiler(StackProfiler.class)
+            .build();
+
+    new Runner(opts).run();
   }
 }
