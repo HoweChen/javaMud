d3d5a30c3c33b000a7456521fcf31c9ad7193ba2
yuhaochen <cyh278889789@gmail.com>
2022-04-01 17:14:49 +0800

    [mud-java-basic] add Guava cache test

--- mud-java-basic/src/test/java/guava/GuavaCacheTest.java
+++ mud-java-basic/src/test/java/guava/GuavaCacheTest.java
@@ -1,6 +1,11 @@
 package guava;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 
 public class GuavaCacheTest {
@@ -14,5 +19,29 @@ public class GuavaCacheTest {
             return null;
           }
         };
+  }
+
+  @Test
+  public void givenCacheWithExpireTime_whenOnTimeAndCleanUp_thenShouldPrintRemovalMessage()
+      throws InterruptedException {
+    Cache<String, String> cache =
+        CacheBuilder.newBuilder()
+            .expireAfterWrite(10, TimeUnit.SECONDS)
+            .removalListener(
+                (RemovalListener<String, String>)
+                    notification ->
+                        System.out.printf(
+                            "Cache removal: [%s]:[%s]%n",
+                            notification.getKey(), notification.getValue()))
+            .build();
+
+    synchronized (Thread.currentThread()) {
+      for (int i = 0; i < 10_000; i++) {
+        cache.put(String.valueOf(i), "haha" + i);
+      }
+
+      TimeUnit.SECONDS.sleep(10);
+      cache.cleanUp();
+    }
   }
 }
