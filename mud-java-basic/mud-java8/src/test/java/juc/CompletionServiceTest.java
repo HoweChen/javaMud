bd7045b719dc0bd3440ffa91956ca132a6ef7b14
yuhaochen <cyh278889789@gmail.com>
2022-05-19 10:46:47 +0800

    [mud-java-basic] add CompletionServiceTest for mud-java8

--- mud-java-basic/mud-java8/src/test/java/juc/CompletionServiceTest.java
+++ mud-java-basic/mud-java8/src/test/java/juc/CompletionServiceTest.java
@@ -1,6 +1,11 @@
 package juc;
 
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import java.sql.Time;
 import java.util.Arrays;
 import java.util.List;
@@ -161,5 +166,101 @@ public class CompletionServiceTest {
     // then
     assert result != null;
     System.out.println(result);
+  }
+
+  @Test
+  public void givenListenableFuture_whenRun_thenShouldReturnSucceed() {
+    ExecutorService executor = Executors.newFixedThreadPool(5);
+    ListeningExecutorService guavaExecutor = MoreExecutors.listeningDecorator(executor);
+    ListenableFuture<String> future1 =
+        guavaExecutor.submit(
+            () -> {
+              // step 1
+              System.out.println("执行step 1");
+              return "step1 result";
+            });
+    ListenableFuture<String> future2 =
+        guavaExecutor.submit(
+            () -> {
+              // step 2
+              System.out.println("执行step 2");
+              return "step2 result";
+            });
+    ListenableFuture<List<String>> future1And2 = Futures.allAsList(future1, future2);
+    Futures.addCallback(
+        future1And2,
+        new FutureCallback<List<String>>() {
+          @Override
+          public void onSuccess(List<String> result) {
+            System.out.println(result);
+            ListenableFuture<String> future3 =
+                guavaExecutor.submit(
+                    () -> {
+                      System.out.println("执行step 3");
+                      return "step3 result";
+                    });
+            Futures.addCallback(
+                future3,
+                new FutureCallback<String>() {
+                  @Override
+                  public void onSuccess(String result) {
+                    System.out.println(result);
+                  }
+
+                  @Override
+                  public void onFailure(Throwable t) {}
+                },
+                guavaExecutor);
+          }
+
+          @Override
+          public void onFailure(Throwable t) {}
+        },
+        guavaExecutor);
+  }
+
+  @Test
+  public void givenCompletableFuture_whenRun_thenShouldReturnSucceed() {
+    ExecutorService executor = Executors.newFixedThreadPool(5);
+    CompletableFuture<String> cf1 =
+        CompletableFuture.supplyAsync(
+            () -> {
+              System.out.println("执行step 1");
+              return "step1 result";
+            },
+            executor);
+    CompletableFuture<String> cf2 =
+        CompletableFuture.supplyAsync(
+            () -> {
+              System.out.println("执行step 2");
+              return "step2 result";
+            });
+    cf1.thenCombine(
+            cf2,
+            (result1, result2) -> {
+              System.out.println(result1 + " , " + result2);
+              System.out.println("执行step 3");
+              return "step3 result";
+            })
+        .thenAccept(result3 -> System.out.println(result3));
+  }
+
+  @Test
+  public void givenThreeCFs_whenAllTrigger_thenShouldSucceed() {
+    CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> "1");
+    CompletableFuture<String> cf2 = CompletableFuture.supplyAsync(() -> "2");
+    CompletableFuture<String> cf3 = CompletableFuture.supplyAsync(() -> "3");
+
+    CompletableFuture<String> allResult =
+        CompletableFuture.allOf(cf1, cf2, cf3)
+            .thenApply(
+                unused -> {
+                  String oneResult = cf1.join();
+                  String twoResult = cf2.join();
+                  String threeResult = cf3.join();
+                  return oneResult + twoResult + threeResult;
+                });
+
+    System.out.println(allResult.join());
   }
 }
